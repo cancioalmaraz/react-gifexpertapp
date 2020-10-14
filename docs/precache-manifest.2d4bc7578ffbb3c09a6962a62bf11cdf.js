2ddae4181ffcd74cdf0c4c2f920db496dd0d52d7
David Almaraz <cancioalmaraz@gmail.com>
2020-10-14 18:03:42 -0400

    Docs add

---
+++ docs/precache-manifest.2d4bc7578ffbb3c09a6962a62bf11cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0887194a434a73d12f515191be256d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c68d4364db2472e3771",
+    "url": "/static/css/main.a4ca3ba7.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c68d4364db2472e3771",
+    "url": "/static/js/main.2c53d8f8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
